2e5db631f912c10aa1d8c4e9b0ff180a57f5296d
gabrielhzgr <gabrielhzgr@gmail.com>
2025-12-26 14:41:12 -0600

    feat: Infinite slider for home page

--- src/Home/InfiniteSlider.jsx
+++ src/Home/InfiniteSlider.jsx
@@ -5,28 +5,30 @@ export default function InfiniteSlider({title="Slider"}){
         <div id="slider">
             <h2>{title}</h2>
             <div className="images">
-                <Image url={image}/>
-                <Image url={image}/>
-                <Image url={image}/>
+                <Image url={image} posNo={0}/>
+                <Image url={image} posNo={1}/>
+                <Image url={image} posNo={2}/>
             </div>
         </div>
     )
 }
 
-function Image({url}){
+function Image({url, posNo=1}){
 
     const imgRef = useRef(null)
     
     useEffect(()=>{
+        
         setInterval(()=>{
             const parentPos = imgRef.current.parentElement.getBoundingClientRect()
             const imgPos = imgRef.current.getBoundingClientRect()
 
             if(imgPos.left+imgRef.current.width <= parentPos.left){
-                imgRef.current.style.right = `-${parentPos.width}px`
+                let offset = (imgRef.current.width + 20) * posNo
+                imgRef.current.style.right = `${-parentPos.width+offset}px`
             }else{
                 let currentRight =  Number(imgRef.current.style.right.replace("px",""))
-                imgRef.current.style.right = `${currentRight+3}px`
+                imgRef.current.style.right = `${currentRight+1}px`
             }
         },30)
     },[])
