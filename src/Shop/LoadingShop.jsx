a0e25128dfa9c88a0307e661a421c6d3c2dd113a
gabrielhzgr <gabrielhzgr@gmail.com>
2026-01-02 14:14:16 -0600

    fix: Remove skeleton animation around articles

--- src/Shop/LoadingShop.jsx
+++ src/Shop/LoadingShop.jsx
@@ -2,7 +2,7 @@ export default function LoadingShop(){
     return (
         <>
            <input name="q" className="skeleton" type="text" />
-            <section data-testid="loading-shop"className="skeleton"> 
+            <section data-testid="loading-shop" className="loading-shop"> 
                 <article className="skeleton"></article>
                 <article className="skeleton"></article>
                 <article className="skeleton"></article>
